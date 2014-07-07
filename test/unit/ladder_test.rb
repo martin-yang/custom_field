c2717747bb7110742dfa0bf62ef705742a4ac987
martin-yang <yangjunfeng@yangjunfengtekiMacBook-Air.local>
2014-07-07 15:03:49 +0800

    build ladder add a item in ladder_test.rb file

--- test/unit/ladder_test.rb
+++ test/unit/ladder_test.rb
@@ -3,9 +3,14 @@ require "custom_field/ladder"
 class LadderTest < Test::Unit::TestCase
   def setup
     @ladder=CustomField::Ladder.build "TestLadder"
+    @ladder.item "TestItem"
   end
 
   def test_should_have_a_ladder_of_name_is_testladder
     assert_equal "TestLadder",@ladder.name
+  end
+
+  def test_should_have_a_item
+    assert_equal 1,@ladder.items
   end
 end
